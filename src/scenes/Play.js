db79b64784a55e2ec1b130a2852c80b50bb9fc88
FeiPetr <777spetrova@gmail.com>
2023-05-18 18:32:45 -0700

    enemy goes across screen (collider of enemy and tower currently not working)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,7 +9,8 @@ class Play extends Phaser.Scene{ //creating js class 'menu' that extends phaser'
         this.load.image('character', './assets/rocket.png');
         // load images/tile sprites
         this.load.image('map', './assets/Test Map.png');
-        this.load.image('tower', './assets/tower_placehold.png');
+        this.load.image('tower', './assets/tower_placeholder.png');
+        this.load.image('enemy', './assets/enemy_placeholder.png');
       }
       
     create(){
@@ -17,8 +18,9 @@ class Play extends Phaser.Scene{ //creating js class 'menu' that extends phaser'
         this.starfield = this.add.tileSprite(0, 0, 1280, 1281, 'map').setOrigin(0, 0);
         // add character (placeholder sprite)
         this.character = this.physics.add.sprite(this.sys.game.config.width / 2 + 50, this.sys.game.config.height/2, 'character',0);
+        this.enemy = this.physics.add.sprite(this.sys.game.config.width, this.sys.game.config.height/2, 'enemy');
         //animate character
-        
+
         /*this.anims.create({
           key: 'cat',
           frames: this.anims.generateFrameNumbers('character', { start: 0, end: 1, first: 0}),
@@ -27,9 +29,11 @@ class Play extends Phaser.Scene{ //creating js class 'menu' that extends phaser'
         });
         this.character.anims.play('cat');*/
 		    this.tower = this.physics.add.staticSprite(this.sys.game.config.width / 2, this.sys.game.config.height/2, 'tower',0);
-        this.towerHP = 500; // set an HP amount for crystal tower
+        this.towerHP = 500; // set an HP amount for crystal tower; when the enemies attack it, this will go down
+        // Maybe add an HP bar? Currently not sure how to code that
 
         this.physics.add.collider(this.character,this.tower);
+        this.physics.add.collider(this.enemy,this.tower); // enemy and character collide with towers (but not each other currently)
         this.character.body.setCollideWorldBounds(true);
 
 
@@ -43,28 +47,29 @@ class Play extends Phaser.Scene{ //creating js class 'menu' that extends phaser'
 
      }
      update() {
-        // check key input for restart
         
+    
         this.direction = new Phaser.Math.Vector2(0)
         if(this.cursors.left.isDown)
         {
-            this.direction.x = -1
+            this.direction.x = -1;
         }
         else if (this.cursors.right.isDown)
         {
-            this.direction.x = 1
+            this.direction.x = 1;
         }
 
         if (this.cursors.up.isDown)
         {
-            this.direction.y = -1
+            this.direction.y = -1;
         }
         else if (this.cursors.down.isDown)
         {
-            this.direction.y = 1
+            this.direction.y = 1;
         }
-        this.direction.normalize()
-        this.character.setVelocity(this.VEL*this.direction.x,this.VEL*this.direction.y)
+        this.direction.normalize();
+        this.character.setVelocity(this.VEL*this.direction.x,this.VEL*this.direction.y);
+        this.enemy.x -= 1;
 
       }
       
